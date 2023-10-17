0e69328b9fca6e441fbe3c25bb6ab6427c0e53f5
林君潔 <linjunjie@linjunjiedeMacBook-Air.local>
2023-10-17 09:09:41 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4f3cc9",
+        "primary-dark": "#4232aa",
+        secondary: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
